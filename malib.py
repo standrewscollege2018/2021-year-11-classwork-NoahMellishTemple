97b99e49382276030138cfb519c45381668af8a6
NoahMellishTemple <78179523+NoahMellishTemple@users.noreply.github.com>
2021-02-02 14:14:50 +1300

    Exucse letter

--- malib.py
+++ malib.py
@@ -1,3 +1,19 @@
-print("To whom this may concern")
-print("please excuse" , name)
-name = input("name")
+#This is a Madlib, getting the user to enter the details
+#Then it prints the story
+
+#Gets their details
+subject = input("What subject are you doing")
+name = input("what is your name?")
+sickness = input("what is your illness?")
+body = input("what part of you body is sore?")
+days = input("how many days will you be away for?")
+year = input("what year are you?")
+parent = input("what parent is writing this letter")
+
+print("To whom this may concern,")
+print("Please excuse" , name , "from" , year , subject , "today")
+print("The muppet hurt his" , body , "playing sport and now has a" , sickness)
+print("He will be back in {} days, but untill then , name , will not be able to attend , year , subject")
+print("Very amused {}s" parent)
+
+.format(days , parent)
