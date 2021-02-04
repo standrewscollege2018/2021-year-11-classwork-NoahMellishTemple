2c98b8e05519a135b25f680246c426dc210814f3
NoahMellishTemple <78179523+NoahMellishTemple@users.noreply.github.com>
2021-02-04 17:05:40 +1300

    Marking thing

---
+++ DTIC.py
@@ -0,0 +1,9 @@
+mark = int(input("what mark did you get"))
+if mark >= 90:
+    print("Congrates you got A")
+elif mark >= 70:
+    print("Congrates you got B")
+elif mark >= 50:
+    print("Congrates you got C")
+else:
+    print("Unlucky you failed")
