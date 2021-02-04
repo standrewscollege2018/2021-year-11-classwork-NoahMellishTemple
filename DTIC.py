737484cb2c182ddf36759f5d77f22b449d426fec
NoahMellishTemple <78179523+NoahMellishTemple@users.noreply.github.com>
2021-02-05 10:13:23 +1300

    Update DTIC.py

--- DTIC.py
+++ DTIC.py
@@ -1,9 +1,14 @@
+#This program calulates grades
+#Set up cut scores
 mark = int(input("what mark did you get"))
-if mark >= 90:
-    print("Congrates you got A")
-elif mark >= 70:
-    print("Congrates you got B")
-elif mark >= 50:
-    print("Congrates you got C")
+if mark >= 0 and mark <= 100:
+    if mark >= 90:
+        print("Congrates you got A")
+    elif mark >= 70:
+        print("Congrates you got B")
+    elif mark >= 50:
+        print("Congrates you got C")
+    else:
+        print("Unlucky you failed")
 else:
-    print("Unlucky you failed")
+    print("Invalid mark")
