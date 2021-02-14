d2ac60db0f7072732475acbd8ae609df84af582e
NoahMellishTemple <78179523+NoahMellishTemple@users.noreply.github.com>
2021-02-15 09:55:55 +1300

    how many guesses

--- game_guess.py
+++ game_guess.py
@@ -1,1 +1,17 @@
-guessed_number = int(input("Choose a number between 1 and 100")
+import random
+number = random.randint(1,10)
+keep_asking = True
+counter = 0
+while keep_asking == True:
+    counter += 1
+    guess = int(input("Guess a number between 1-10"))
+    if guess == number:
+        keep_asking = False
+    else:
+        print("Wrong number")
+        if guess > number:
+            print("Too high")
+        elif guess < number:
+            print("Too low")
+print("Thats right")
+print("You took {} guesses".format(counter))
