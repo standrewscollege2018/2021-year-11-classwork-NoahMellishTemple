e95a2c8778ccd1b4e1873ac34ce002d4a20b5fbb
NoahMellishTemple <78179523+NoahMellishTemple@users.noreply.github.com>
2021-02-16 14:24:43 +1300

    high score

--- game_guess.py
+++ game_guess.py
@@ -1,17 +1,30 @@
 import random
-number = random.randint(1,10)
-keep_asking = True
-counter = 0
-while keep_asking == True:
-    counter += 1
-    guess = int(input("Guess a number between 1-10"))
-    if guess == number:
-        keep_asking = False
+high_score = 1000
+keep_playing = True
+while keep_playing == True:
+    number = random.randint(1,100)
+    keep_asking = True
+    counter = 0
+
+    while keep_asking == True:
+        counter += 1
+        guess = int(input("Guess a number between 1-100"))
+        if guess == number:
+            keep_asking = False
+        else:
+            print("Wrong number")
+            if guess > number:
+                print("Too high")
+            elif guess < number:
+                print("Too low")
+    if counter <= high_score:
+        print("Congrates you got a high score")
+        high_score = counter
+    print("Thats right")
+    print("You took {} guesses".format(counter))
+    print("The current best score is {}".format(high_score))
+    y_n = input("Do you want to play again y/n")
+    if y_n == "y":
+        keep_playing = True
     else:
-        print("Wrong number")
-        if guess > number:
-            print("Too high")
-        elif guess < number:
-            print("Too low")
-print("Thats right")
-print("You took {} guesses".format(counter))
+        keep_playing = False
